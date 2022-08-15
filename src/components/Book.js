5167729ca10e80d1b203600b701f8d3e405e2eb3
AhmedYoussef <Ahmed@My-HP>
2022-08-15 13:05:05 +0200

    fix: select none if the book is not on any shelf

--- src/components/Book.js
+++ src/components/Book.js
@@ -21,9 +21,7 @@ function Book({ book, addToShelf }) {
           ></div>
           <div className="book-shelf-changer">
             <select value={book.shelf || "none"} onChange={handleChange}>
-              <option value="none" disabled>
-                Move to...
-              </option>
+              <option disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
